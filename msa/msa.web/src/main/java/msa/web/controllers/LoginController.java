f1572a77ee7988562233026e1c6446672e28eb5c
Giovanni Bottalico <giovanni.bottalico@finconsgroup.local>
2017-07-19 16:30:57 +0200

    aggiunto esempio accesso a Mongo

--- msa/msa.web/src/main/java/msa/web/controllers/LoginController.java
+++ msa/msa.web/src/main/java/msa/web/controllers/LoginController.java
@@ -1,10 +1,11 @@
-package msa.web.controllers;
+ package msa.web.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
