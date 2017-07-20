18d8229f9e51b04f1d70db47e80ea177133b73ce
Giovanni Bottalico <giovanni.bottalico@finconsgroup.local>
2017-07-20 16:15:08 +0200

    eliminazione bower component e node module

--- msa-frontend/dist/scripts/services/cmpsvc/account-user-svc.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('msa').service('AccountUserSvc', ['$http', 'msaServicesApiUrls', '$sessionStorage', '$cookieStore', '$rootScope', '$q', '$timeout', '$log', function ($http, msaServicesApiUrls, $sessionStorage, $cookieStore, $rootScope, $q, $timeout, $log) {
-
-		var $svc = this;
-}]);
