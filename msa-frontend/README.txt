5351e7f2a71967a781001e0507fc932a33e8de75
Giacomo Rossi <giacomo.r@open-knowledge.it>
2017-07-21 16:34:28 +0200

    fix errori gulp js

--- msa-frontend/README.txt
+++ msa-frontend/README.txt
@@ -34,17 +34,38 @@ http://itaswkf.gruppoitas.local/mediawiki/index.php?title=Frontend
 /************** Setup ENVIRONMENT ****************/
 /*************************************************/
 
+    @Linux && OS
+
+    # Installare dipendenze npm
+    sudo npm install
+
+    # Installare Bower
+    sudo npm install bower
+    sudo npm install -g bower
+
+    # Installare moduli Bower
+    sudo bower install
+
+    # Installare Gulp
+    sudo npm install gulp
+    sudo npm install -g gulp
+
+
+    @Windows lanciare i comandi da cmd
+
     # Installare dipendenze npm
     npm install
 
     # Installare Bower
-    sudo npm install -g bower@1.6.5
+    npm install bower
+    npm install -g bower
 
     # Installare moduli Bower
     bower install
 
     # Installare Gulp
-    sudo npm install -g gulp@3.9.1
+    npm install gulp
+    npm install -g gulp
 
 /@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/
 /@@@@@@@@@@@  END Setup ENVIRONMENT   @@@@@@@@@@@@/
--- msa-frontend/app/component/common/header/components/header-cmp.js
+++ msa-frontend/app/component/common/header/components/header-cmp.js
@@ -1,13 +1,16 @@
-"use strict";
+(function () {
+	"use strict";
 
-app.component('msaHeader', {
-    templateUrl: '../../app/component/common/header/components/templates/header-tpl.html',
-    bindings: {},
-    controller: ("headerController", ['$rootScope', '$translate', '$log', 'AccountUserSvc', 'toastr', '$analytics', '$location', '$cookies', '$window', '$sessionStorage', 
-        function($rootScope, $translate, $log, AccountUserSvc, toastr, $analytics, location, $cookies, $window, $sessionStorage) {
-            
-        var ctrl = this;
+	app.component('msaHeader', {
+	    templateUrl: '../../app/component/common/header/components/templates/header-tpl.html',
+	    bindings: {},
+	    controller: ("headerController", ['$rootScope', '$translate', '$log', 'AccountUserSvc', 'toastr', '$analytics', '$location', '$cookies', '$window', '$sessionStorage', 
+	        function($rootScope, $translate, $log, AccountUserSvc, toastr, $analytics, location, $cookies, $window, $sessionStorage) {
+	            
+	        var ctrl = this;
 
-    }])
-});
+	    }])
+	});
+
+}());
  
