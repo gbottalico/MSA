4f8f3ea54d6ec62a64d2315e0c141fdce891090e
Giacomo Rossi <giacomo.r@open-knowledge.it>
2017-07-26 12:34:44 +0200

    implementazione controlli come widget per campi input e radio button ma non per liste

--- msa-frontend/app/component/home/polizza-search/components/polizza-search-cmd.js
+++ msa-frontend/app/component/home/polizza-search/components/polizza-search-cmd.js
@@ -9,6 +9,50 @@
 	            
 	        var ctrl = this;
 
+	        var listacompagnie = {
+	        	compagnia1: 'Prova 1',
+	        	compagnia2: 'Prova 2',
+	        	compagnia3: 'Prova 3',
+	        	compagnia4: 'Prova 4',
+	        	compagnia5: 'Prova 5',
+	        	compagnia6: 'Prova 6',
+	        	compagnia7: 'Prova 7',
+	        	compagnia8: 'Prova 8',
+	        	compagnia9: 'Prova 9'
+	        };
+
+	        ctrl.ricercapolizza = {
+	        	cognome: '',
+	        	nome: '',
+	        	tipopersona: undefined,
+	        	numpoli: '',
+	        	numsin: '',
+	        	dataevento: '',
+	        	targa: '',
+	        	numprov: '',
+	        	numpre: '',
+	        	compagnie: listacompagnie,
+	        	compagniaselected: undefined,
+	        };
+
+	        ctrl.compagniaSelected = function(compagnia) {
+	        	ctrl.ricercapolizza.compagniaselected = compagnia;
+	        	ctrl.open.compagnia = !ctrl.open.compagnia;
+	        };
+
+	        ctrl.valoriricerca = {
+	        	cognome: ctrl.ricercapolizza.cognome,
+	        	nome: ctrl.ricercapolizza.nome,
+	        	tipopersona: ctrl.ricercapolizza.tipopersona,
+	        	numpoli: ctrl.ricercapolizza.numpoli,
+	        	numsin: ctrl.ricercapolizza.numsin,
+	        	dataevento: ctrl.ricercapolizza.dataevento,
+	        	targa: ctrl.ricercapolizza.targa,
+	        	numprov: ctrl.ricercapolizza.numprov,
+	        	numpre: ctrl.ricercapolizza.numpre,
+	        	compagnia: ctrl.ricercapolizza.compagniaselected
+	        };
+
 	    }])
 	});
 	
