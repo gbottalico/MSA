4e7f4d7553ddef4bc6a1a4fb5e7f74f49badace1
simon.calabrese <bartolomeomio8>
2017-08-02 09:28:00 +0200

    create method for mongo value

--- msa/msa.infrastructure/src/main/java/msa/infrastructure/base/repository/domain/BaseRepository.java
+++ msa/msa.infrastructure/src/main/java/msa/infrastructure/base/repository/domain/BaseRepository.java
@@ -1,15 +1,19 @@
 package msa.infrastructure.base.repository.domain;
 
 import msa.infrastructure.config.AbstractMsaPropertiesReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import msa.domain.Converter.MsaConverter;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.query.Query;
 
 
 public class BaseRepository {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BaseRepository.class);
 	@Autowired 
 	protected MsaConverter converter;
 
@@ -22,5 +26,14 @@ public class BaseRepository {
 	protected Query getCriteriaQueryBuilder() {
 		thisQuery = new Query();
 		return thisQuery;
+	}
+
+	protected <T>  String getMongoNameByAttributeName(final String attributeName, Class<T> dboClass) {
+		try {
+			return dboClass.getDeclaredField(attributeName).getAnnotation(Field.class).value();
+		} catch (NoSuchFieldException e) {
+			LOGGER.error(e.getMessage());
+			return null;
+		}
 	}
 }
