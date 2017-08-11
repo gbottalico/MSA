0cff5b7590cbb4ad7a994b74cf9cad069626ae6e
giacomo rossi <j.s.13@hotmail.it>
2017-08-11 12:29:19 +0200

    fix errori gulp e text area con limitatore e avviso numero massimo di caratteri

--- msa-frontend/app/component/denuncia-sinistro/auto/components/auto-cmp.js
+++ msa-frontend/app/component/denuncia-sinistro/auto/components/auto-cmp.js
@@ -28,118 +28,118 @@
                 ctrl.bottomrightb = false;
 
                 ctrl.indicatoretoplefta = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.toplefta = true;
                     } else {
                         ctrl.toplefta = false;
                     }
-                }
+                };
                 ctrl.indicatoretopcentera = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.topcentera = true;
                     } else {
                         ctrl.topcentera = false;
                     }
-                }
+                };
                 ctrl.indicatoretoprighta = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.toprighta = true;
                     } else {
                         ctrl.toprighta = false;
                     }
-                }
+                };
                 ctrl.indicatoremiddlelefta = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.middlelefta = true;
                     } else {
                         ctrl.middlelefta = false;
                     }
-                }
+                };
                 ctrl.indicatoremiddlerighta = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.middlerighta = true;
                     } else {
                         ctrl.middlerighta = false;
                     }
-                }
+                };
                 ctrl.indicatorebottomlefta = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.bottomlefta = true;
                     } else {
                         ctrl.bottomlefta = false;
                     }
-                }
+                };
                 ctrl.indicatorebottomcentera = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.bottomcentera = true;
                     } else {
                         ctrl.bottomcentera = false;
                     }
-                }
+                };
                 ctrl.indicatorebottomrighta = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.bottomrighta = true;
                     } else {
                         ctrl.bottomrighta = false;
                     }
-                }
+                };
 
                 ctrl.indicatoretopleftb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.topleftb = true;
                     } else {
                         ctrl.topleftb = false;
                     }
-                }
+                };
                 ctrl.indicatoretopcenterb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.topcenterb = true;
                     } else {
                         ctrl.topcenterb = false;
                     }
-                }
+                };
                 ctrl.indicatoretoprightb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.toprightb = true;
                     } else {
                         ctrl.toprightb = false;
                     }
-                }
+                };
                 ctrl.indicatoremiddleleftb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.middleleftb = true;
                     } else {
                         ctrl.middleleftb = false;
                     }
-                }
+                };
                 ctrl.indicatoremiddlerightb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.middlerightb = true;
                     } else {
                         ctrl.middlerightb = false;
                     }
-                }
+                };
                 ctrl.indicatorebottomleftb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.bottomleftb = true;
                     } else {
                         ctrl.bottomleftb = false;
                     }
-                }
+                };
                 ctrl.indicatorebottomcenterb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.bottomcenterb = true;
                     } else {
                         ctrl.bottomcenterb = false;
                     }
-                }
+                };
                 ctrl.indicatorebottomrightb = function(lato) {
-                    if (lato == false) {
+                    if (lato === false) {
                         ctrl.bottomrightb = true;
                     } else {
                         ctrl.bottomrightb = false;
                     }
-                }
+                };
 
         }])
     });
