a147af3bb24e3774136209e06ca8f54e95d981cc
simon.calabrese <bartolomeomio8>
2017-08-23 09:24:04 +0200

    handle exception

--- msa/msa.application/src/main/java/msa/application/service/interfaceDispatcher/DispatcherUtils.java
+++ msa/msa.application/src/main/java/msa/application/service/interfaceDispatcher/DispatcherUtils.java
@@ -3,13 +3,9 @@ package msa.application.service.interfaceDispatcher;
 import msa.application.exceptions.InternalMsaException;
 import msa.application.service.base.BaseService;
 import msa.application.service.sinistri.MsaFunction;
-import msa.domain.Converter.FunctionUtils;
 import msa.domain.object.dispatcher.DispatcherDO;
-import msa.domain.object.sinistro.BaseSinistroDO;
-import msa.domain.object.sinistro.SinistroDO;
 import msa.infrastructure.base.repository.domain.CasaRegoleBaseRepository;
 import msa.infrastructure.repository.SinistriRepository;
-import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.HashMap;
@@ -32,22 +28,32 @@ public class DispatcherUtils extends BaseService {
     private class ComplexTree {
         private Map<String, MsaFunction<Map<String, String>, String>> tree;
 
-        private MsaFunction<Map< String, String>, String> M23Tree = map -> casaRegoleBaseRepository.findAll().stream()
-                .filter(e -> e.getIdCompagnia().equals(map.get("compagnia")))
-                .map(e -> e.getIncaricoPerito() ? "0" : "1").findFirst().orElse(null);
+        private MsaFunction<Map< String, String>, String> M23Tree = map -> {
+            try {
+                return casaRegoleBaseRepository.findAll().stream()
+                        .filter(e -> e.getIdCompagnia().equals(map.get("compagnia")))
+                        .map(e -> e.getIncaricoPerito() ? "0" : "1").findFirst().orElse(null);
+            }catch (Exception e) {
+                throw new InternalMsaException();
+            }
+        };
         private MsaFunction<Map< String, String>, String> M22Tree = map -> {
-            String param = casaRegoleBaseRepository.findAll().stream()
-                    .filter(e -> e.getIdCompagnia().equals(map.get("compagnia")))
-                    .map(e -> {
-                        if (e.getCarrozzeriaConvenzionata()) {
-                            return "0";
-                        } else if (!e.getCarrozzeriaConvenzionata() && e.getIncaricoPerito()) {
-                            return "1";
-                        } else {
-                            return "2";
-                        }
-                    }).findFirst().orElse(null);
-            return param;
+           try {
+               String param = casaRegoleBaseRepository.findAll().stream()
+                       .filter(e -> e.getIdCompagnia().equals(map.get("compagnia")))
+                       .map(e -> {
+                           if (e.getCarrozzeriaConvenzionata()) {
+                               return "0";
+                           } else if (!e.getCarrozzeriaConvenzionata() && e.getIncaricoPerito()) {
+                               return "1";
+                           } else {
+                               return "2";
+                           }
+                       }).findFirst().orElse(null);
+               return param;
+           } catch (Exception e) {
+               throw new InternalMsaException();
+           }
         };
 
         private ComplexTree() {
