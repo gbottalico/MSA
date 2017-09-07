4dc96e3debb1d58fac7b37e85c94698268d8dd70
simon.calabrese <bartolomeomio8>
2017-09-07 17:51:41 +0200

    Configurato Application Server Tomcat per ricevere file di qualsiasi tipo fino a 100MB

---
+++ msa/msa.config/src/main/java/msa/config/MultipartConfig.java
@@ -0,0 +1,21 @@
+package msa.config;
+
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * Created by simon.calabrese on 07/09/2017.
+ */
+@Configuration
+public class MultipartConfig {
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("100MB");
+        factory.setMaxRequestSize("100MB");
+        return factory.createMultipartConfig();
+    }
+}
