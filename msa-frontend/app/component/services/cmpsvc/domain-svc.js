00f32e7db937332399dec095279e966c6496e5ca
Corrado dello Russo <info@corradodellorusso.it>
2017-09-11 12:05:28 +0200

    - Nuova implementazione debugsvc; - Strategia di caching dominio.

--- msa-frontend/app/component/services/cmpsvc/domain-svc.js
+++ msa-frontend/app/component/services/cmpsvc/domain-svc.js
@@ -20,15 +20,25 @@ angular.module('msa').service(
             };
 
             $svc.getElencoRegole = function () {
-                //TODO: elaborare un metodo per cachare.
+                // TODO estendere a tutti la cache.
+                var deferred = $q.defer();
                 if ($rootScope.domain.casaRegole) {
-                    DebugSvc.log("getElencoRegole, using cache.");
-                    return UtilSvc.createPromise($rootScope.domain.casaRegole);
-                    //return UtilSvc.createPromise([{idCompagnia: "37", descrizioneCompagnia: "Cache"}]);
-                } else {
-                    DebugSvc.log("getElencoRegole, using $http.");
-                    return $http.get(msaServicesApiUrls.casaregole);
+                    deferred.resolve($rootScope.domain.casaRegole);
+                    DebugSvc.log("getElencoRegole, returing cached.", $rootScope.domain.casaRegole);
+                    return deferred.promise;
                 }
+
+                $http.get(msaServicesApiUrls.casaregole).then(function(response){
+                    DebugSvc.log("getElencoRegole, returing $http", response.data);
+                    if(response.status === 200 && response.data.status === 200) {
+                        deferred.resolve(response.data);
+                        $rootScope.domain.casaRegole = response.data;
+                    }
+                }, function(data, status, headers, config) {
+                    deferred.reject("Error: request returned status " + status);
+                });
+
+                return deferred.promise;
 
             };
 
@@ -60,17 +70,13 @@ angular.module('msa').service(
                     DebugSvc.log("Returning cached.");
                     return deferred.promise;
                 }
-                // $http.get(msaServicesApiUrls.casaregole + "GGGGG").then(function(data){
-                //     deferred.resolve(data.result);
-                //     $rootScope.domain.casaRegole = data.result;
-                // }, function(data, status, headers, config) {
-                //     deferred.reject("Error: request returned status " + status);
-                // });
-                $http.get(msaServicesApiUrls.casaregole + "GGGGG").then(function successCallback(response) {
-                    DebugSvc.log("successCallback");
-                }, function errorCallback(response) {
-                    DebugSvc.log("errorCallback");
+                $http.get(msaServicesApiUrls.casaregole + "GGGGG").then(function(data){
+                    deferred.resolve(data.result);
+                    $rootScope.domain.casaRegole = data.result;
+                }, function(data, status, headers, config) {
+                    deferred.reject("Error: request returned status " + status);
                 });
+
                 return deferred.promise;
             };
 
