1cebef681a6a0cf518c534e8e282d481651c3584
Corrado dello Russo <info@corradodellorusso.it>
2017-09-13 08:42:03 +0200

    - Validazione CAI; - Code review.

--- msa-frontend/app/component/denuncia-sinistro/riepilogo/components/riepilogo-cmp.js
+++ msa-frontend/app/component/denuncia-sinistro/riepilogo/components/riepilogo-cmp.js
@@ -4,8 +4,8 @@
     app.component('msaRiepilogo', {
         templateUrl: '../../app/component/denuncia-sinistro/riepilogo/components/templates/riepilogo-tpl.html',
         bindings: {},
-        controller: ("riepilogoController", ['_', '$rootScope', '$uibModal','$scope', '$routeParams', '$debugMode', '$filter', '$location', 'toastr', 'SinistriSvc', 'DebugSvc', 'PathSvc', 'PlacesSvc',
-            function (_, $rootScope, $scope, $routeParams, $debugMode,$uibModal, $filter, $location, toastr, SinistriSvc, DebugSvc, PathSvc, PlacesSvc) {
+        controller: ("riepilogoController", ['_', '$rootScope', '$uibModal', '$scope', '$routeParams', '$debugMode', '$filter', '$location', 'toastr', 'SinistriSvc', 'DebugSvc', 'PathSvc', 'PlacesSvc',
+            function (_, $rootScope, $scope, $routeParams, $debugMode, $uibModal, $filter, $location, toastr, SinistriSvc, DebugSvc, PathSvc, PlacesSvc) {
 
                 var $ctrl = this;
                 var $translate = $filter('translate');
@@ -19,14 +19,14 @@
                         var result = response.data.result;
                         $ctrl.nomeComune = result;
 
-                    })
+                    });
                 };
                 $ctrl.getSinistroProvvisorio = function (numeroSinistroProvvisorio) {
                     SinistriSvc.cercaSinistroProvvisorio(numeroSinistroProvvisorio).then(function (response) {
                         var result = response.data.result;
                         DebugSvc.log("getSinistroProvvisorio", response);
                         $ctrl.sinistroProvvisorio = result;
-                        if (_.isObject(result) ) {
+                        if (_.isObject(result)) {
                             $ctrl.getNomeComune(result.segnalazione.codComune);
 
                             $ctrl.bindRiepilogo();
@@ -48,38 +48,34 @@
                     $ctrl.nomeCognome = $ctrl.sinistroProvvisorio.segnalazione.denunciante.nome + " " + $ctrl.sinistroProvvisorio.segnalazione.denunciante.cognome;
                     $ctrl.luogoSinistro = $ctrl.sinistroProvvisorio.segnalazione.indirizzo;
                     //TODO VEDERE PERCHE E UNDEFINED
-                    if($ctrl.nomeComune!==undefined) {
-                      $ctrl.luogoSinistro = $ctrl.luogoSinistro + ", "+  $ctrl.nomeComune;
+                    if ($ctrl.nomeComune !== undefined) {
+                        $ctrl.luogoSinistro = $ctrl.luogoSinistro + ", " + $ctrl.nomeComune;
                     }
-                   $ctrl.cellulare = $ctrl.sinistroProvvisorio.segnalazione.denunciante.tracking.cellulare;
+                    $ctrl.cellulare = $ctrl.sinistroProvvisorio.segnalazione.denunciante.tracking.cellulare;
                     $ctrl.email = $ctrl.sinistroProvvisorio.segnalazione.denunciante.tracking.mail;
                     $ctrl.numveicoli = $ctrl.sinistroProvvisorio.eventoRca.numVeicoli;
                     //TODO PER LA GARANZIA BISOGNA VEDERE LATO MSA COME VERRA' GESTITA
-                    $ctrl.garanzia= $ctrl.sinistroProvvisorio.segnalazione.garanziaSelected;
-                    if($ctrl.sinistroProvvisorio.eventoRca.codAutorita === null){
+                    $ctrl.garanzia = $ctrl.sinistroProvvisorio.segnalazione.garanziaSelected;
+                    if ($ctrl.sinistroProvvisorio.eventoRca.codAutorita === null) {
                         $ctrl.autorita = false;
                     }
                     else {
                         $ctrl.autorita = true;
                     }
 
-                  $ctrl.constatazione = $ctrl.sinistroProvvisorio.constatazioneAmichevole.caCompilata;
+                    $ctrl.constatazione = $ctrl.sinistroProvvisorio.constatazioneAmichevole.caCompilata;
 
-                        $ctrl.legali = $ctrl.sinistroProvvisorio.legali;
-
-
+                    $ctrl.legali = $ctrl.sinistroProvvisorio.legali;
 
 
-
-
-                    if($ctrl.sinistroProvvisorio.dannoRca.anagraficaDanniCliente === undefined){
+                    if ($ctrl.sinistroProvvisorio.dannoRca.anagraficaDanniCliente === undefined) {
                         $ctrl.conducente = $ctrl.sinistroProvvisorio.contraente;
                         $ctrl.targaConducente = $ctrl.sinistroProvvisorio.targa;
                     }
                     else $ctrl.conducente = $ctrl.sinistroProvvisorio.dannoRca.anagraficaDanniCliente;
 
 
-                    if($ctrl.sinistroProvvisorio.eventoRca.numVeicoli > 1){
+                    if ($ctrl.sinistroProvvisorio.eventoRca.numVeicoli > 1) {
                         $ctrl.contropartePresente = true;
                         $ctrl.controparti = $ctrl.sinistroProvvisorio.dannoRca.anagraficaDanniControparte;
                     }
