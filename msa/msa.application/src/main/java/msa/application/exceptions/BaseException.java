d10898af41ccb0918109b5b33633069a414fc332
simon.calabrese <bartolomeomio8>
2017-09-21 16:58:31 +0200

    fix documenti e gestione errori generici

--- msa/msa.application/src/main/java/msa/application/exceptions/BaseException.java
+++ msa/msa.application/src/main/java/msa/application/exceptions/BaseException.java
@@ -4,7 +4,9 @@ import msa.application.config.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by simon.calabrese on 24/07/2017.
@@ -14,8 +16,13 @@ public class BaseException extends Exception {
     private Exception exceptionThrowed;
     private List<Message> messages;
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseException.class);
+
     public BaseException(Exception exceptionThrowed, List<Message> messages) {
-        LOGGER.error(exceptionThrowed != null ? exceptionThrowed.getMessage() : "");
+        LOGGER.error(exceptionThrowed != null ? Arrays.stream(exceptionThrowed
+                .getStackTrace())
+                .map(StackTraceElement::toString)
+                .collect(Collectors.joining(",\n"))
+                : "");
         this.exceptionThrowed = exceptionThrowed;
         this.messages = messages;
 
