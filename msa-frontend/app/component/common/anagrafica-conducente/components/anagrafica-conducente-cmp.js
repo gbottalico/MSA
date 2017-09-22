7a8e525e872ae36dba7ac713cb8bcffe932fa9cb
Corrado dello Russo <info@corradodellorusso.it>
2017-09-22 11:41:31 +0200

    - Avanzamento componente anagrafica conducente.

--- msa-frontend/app/component/common/anagrafica-conducente/components/anagrafica-conducente-cmp.js
+++ msa-frontend/app/component/common/anagrafica-conducente/components/anagrafica-conducente-cmp.js
@@ -5,48 +5,81 @@
         templateUrl: '../../app/component/common/anagrafica-conducente/components/templates/anagrafica-conducente-tpl.html',
         bindings: {
             result: "=",
+            input: "<",
             name: "<"
         },
         controller: ("anagraficaConducenteController", ['_', '$MSAC', '$scope', 'DebugSvc', 'RegexSvc', 'toastr',
             function (_, $MSAC, $scope, DebugSvc, RegexSvc, toastr) {
 
-            var $ctrl = this;
-            $scope.$MSAC = $MSAC;
-            $scope.$regex = RegexSvc;
+                var $ctrl = this;
+                $scope.$MSAC = $MSAC;
+                $scope.$regex = RegexSvc;
 
-            $ctrl.calcolaCf = function () {
-                var luogoNascita = $ctrl.nascita.comune ?
-                    $ctrl.nascita.comune.descrizione :
-                    $ctrl.nascita.nazione.descrizione;
+                $ctrl.persistence = {};
 
-                UtilSvc.calcolaCf($ctrl.cognome, $ctrl.nome, $ctrl.sesso, $ctrl.nascita.data, luogoNascita).then(function (response) {
-                    DebugSvc.log("calcolaCf", response);
-                    if (response.data.status === 200) {
-                        $ctrl.cf = response.data.result;
-                    } else {
-                        toastr.error($translate('global.generic.cfko'));
+                $ctrl.calcolaCf = function () {
+                    var luogoNascita = $ctrl.nascita.comune ?
+                        $ctrl.nascita.comune.descrizione :
+                        $ctrl.nascita.nazione.descrizione;
+
+                    UtilSvc.calcolaCf($ctrl.cognome, $ctrl.nome, $ctrl.sesso, $ctrl.nascita.data, luogoNascita).then(function (response) {
+                        DebugSvc.log("calcolaCf", response);
+                        if (response.data.status === 200) {
+                            $ctrl.cf = response.data.result;
+                        } else {
+                            toastr.error($translate('global.generic.cfko'));
+                        }
+                    });
+                };
+
+                $ctrl.isCalcolaCfDisabled = function () {
+                    try {
+                        return !($ctrl.cognome && $ctrl.nome && $ctrl.sesso && $ctrl.nascita.data);
+                    } catch (error) {
+                        return false;
                     }
-                });
-            };
+                };
 
-            $ctrl.isCalcolaCfDisabled = function () {
-                try {
-                    return !($ctrl.cognome && $ctrl.nome && $ctrl.sesso && $ctrl.nascita.data);
-                } catch (error) {
-                    return false;
-                }
-            };
+                $ctrl.bind = function () {
+                    
+                    var anagrafica = $ctrl.input;
 
-            $scope.$watch(
-                function watchScope(scope) {
-                    return {};
-                },
-                function handleChanges(newValues, oldValues) {
-                },
-                true
-            );
+                    $ctrl.result.cognome = anagrafica.cognome;
+                    $ctrl.result.nome = anagrafica.nome;
+                    $ctrl.result.sesso = anagrafica.sesso;
+                    $ctrl.result.cf = anagrafica.cf;
+                    $ctrl.result.nascita = {};
 
-        }])
+                    if (anagrafica.dataNascita) {
+                        $ctrl.result.nascita.data = new Date(anagrafica.dataNascita);
+                    }
+
+                    $ctrl.persistence.luogoNascita = anagrafica.luogoNascita;
+
+                    if (_.isObject(anagrafica.tracking)) {
+                        $ctrl.result.telefono = anagrafica.tracking.telefono;
+                        $ctrl.result.mail = anagrafica.tracking.mail;
+                        $ctrl.persistence.residenza = anagrafica.tracking.residenza;
+
+                        $ctrl.result.residenza = {};
+                        $ctrl.result.residenza.indirizzo = anagrafica.tracking.indirizzo;
+                    }  
+                };
+                
+                $ctrl.$doCheck = function () {
+                    DebugSvc.log('doCheck:' + $ctrl.input);
+                };
+
+                $scope.$watch(
+                    function watchScope(scope) {
+                        return {};
+                    },
+                    function handleChanges(newValues, oldValues) {
+                    },
+                    true
+                );
+
+            }])
     });
 
 }());
