b14f4934ca6cb5733fc565cac2dd8602cce2a73f
Corrado dello Russo <info@corradodellorusso.it>
2017-09-25 09:48:53 +0200

    - Refactor modal dettaglio polizza.

--- msa-frontend/app/component/common/dettaglio-polizza-modal/components/dettaglio-polizza-modal-cmp.js
+++ msa-frontend/app/component/common/dettaglio-polizza-modal/components/dettaglio-polizza-modal-cmp.js
@@ -12,10 +12,8 @@
             function ($rootScope, $scope, UtilSvc, DebugSvc, DomainSvc) {
 
                 var $ctrl = this;
-                $ctrl.clearbox = "clear box";
                 $ctrl.polizza = $ctrl.resolve.polizza;
-                              //$ctrl.dettagliopolizza = $rootScope.polizza;
-                DebugSvc.log($ctrl.polizza);
+
                 $ctrl.cancel = function () {
                     $ctrl.dismiss({$value: undefined});
                 };
