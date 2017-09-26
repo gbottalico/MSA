d7f496e19a5b147a23e845c04401b87cbc29daea
Corrado dello Russo <info@corradodellorusso.it>
2017-09-26 11:37:35 +0200

    - Avviso navigazione.

--- msa-frontend/app/component/common/header/components/header-cmp.js
+++ msa-frontend/app/component/common/header/components/header-cmp.js
@@ -1,22 +1,32 @@
 (function () {
-	"use strict";
+    "use strict";
 
-	app.component('msaHeader', {
-	    templateUrl: '../../app/component/common/header/components/templates/header-tpl.html',
-	    bindings: {},
-	    controller: ("headerController", ['$MSAC', '$location',
-	        function($MSAC, $location) {
-	            
-	        var $ctrl = this;
+    app.component('msaHeader', {
+        templateUrl: '../../app/component/common/header/components/templates/header-tpl.html',
+        bindings: {},
+        controller: ("headerController", ['$MSAC', '$location', '$ngBootbox', 'DebugSvc',
+            function ($MSAC, $location, $ngBootbox, DebugSvc) {
 
-			$ctrl.reloadRoute = function() {
-				var path = $MSAC.PATHS.HOME;
-				$location.search({});
-				$location.path(path);
-			};
+                var $ctrl = this;
 
-	    }])
-	});
+                $ctrl.reloadRoute = function () {
+                    var homePath = $MSAC.PATHS.HOME;
+                    var path = $location.path();
+                    if (homePath !== path) {
+                        $ngBootbox.confirm('Sei sicuro di voler tornare alla homepage? i dati non salvati andranno persi.').then(function () {
+                            //TODO formattare + numero sinistro.
+                            $location.search({});
+                            $location.path(homePath);
+                        }, function () {/* NOPE */});
+                    } else {
+                        $location.search({});
+                        $location.path(homePath);
+                    }
+
+                };
+
+            }])
+    });
 
 }());
  
